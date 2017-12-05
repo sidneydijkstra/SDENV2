712bc5c491da6d4d64db9de3b57e4463e8f19c5b
sidney dijkstra <sidney dijkstra>
2017-12-05 09:31:17 +0100

    fix get color in glsl function

--- sdenv2/project/color.cpp
+++ sdenv2/project/color.cpp
@@ -23,7 +23,8 @@ void Color::setColor(glm::vec3 _c){
 }
 
 glm::vec3 Color::getColor(){
-	return glm::vec3(r / 255,g / 255,b / 255);
+	float i = 1.0f / 255.0f;
+	return glm::vec3(i * r, i * g, i * b);
 }
 
 Color Color::lerpColor(Color c1, Color c2, float amount){
